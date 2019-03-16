f3ee2965d3ee29d98784ea449f50e9569a8450de
yaboiloe <48611635+yaboiloe@users.noreply.github.com>
2019-03-16 14:28:40 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeVisibility(){
+  document.getElementById("hidden_steps").style.display="block";
+}
+function changeVisibility2(){
+  document.getElementById("aquaponics")
+  .style.display="block";
+}
+function changeVisibility3(){
+  document.getElementById("plants")
+  .style.display="block";
+}
